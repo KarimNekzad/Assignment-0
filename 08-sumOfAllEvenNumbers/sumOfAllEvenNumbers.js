496e7f928ddd3bb20bf5be8b14d4f12cc0fb814b
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 18:54:51 -0500

    Karim Nekzad 08-sumOfAllEvenNumbers completed

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,6 @@
-function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
-}
+// Karim Nekzad
+// not really a sum, it just counts all even numbers
+let sumOfAllEvenNumbers = nums => nums.filter(number => number % 2 === 0).length
 
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
