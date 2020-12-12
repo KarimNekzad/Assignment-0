4f5b62c5010986644b495269e9411df8457adae6
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 17:30:02 -0500

    Karim Nekzad 04-countOfAllBooleans completed

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,12 @@
-function countOfAllBooleans(arr) {
-  // Insert code here;
-}
+// Karim Nekzad
+
+// let countOfAllBooleans = arr => {
+//   arr = arr.filter(element => element === true || element === false)
+//   return arr.length
+// }
+
+// Shortend version of above ^
+let countOfAllBooleans = arr => arr.filter(element => element === true || element === false).length
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
