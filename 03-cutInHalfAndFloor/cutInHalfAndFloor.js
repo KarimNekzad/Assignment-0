56bb46190e7fc888c1d468d33bf2d607a6a19c5a
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 17:08:59 -0500

    Karim Nekzad 03-cutInHalfAndFloor completed

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,6 +1,5 @@
-function cutInHalfAndFloor(num) {
-  // Insert code here;
-}
+// Karim Nekzad
+let cutInHalfAndFloor = num => Math.floor(num/2)
 
 // Do not edit this line;
 module.exports = cutInHalfAndFloor;
