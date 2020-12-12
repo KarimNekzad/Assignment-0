a396a3e0ceffe888716ace22d2f02275bd3a0c25
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 18:44:06 -0500

    Karim Nekzad 07-countOfAllIndexMatchingNumbers completed

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,14 @@
-function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+// Karim Nekzad
+let countOfAllIndexMatchingNumbers = nums => {
+  let indexMatch = 0
+
+  for(let i=0; i<nums.length; i++) {
+    if(nums[i] === i) {
+      indexMatch++
+    }
+  }
+
+  return indexMatch
 }
 
 // Do not edit this line;
