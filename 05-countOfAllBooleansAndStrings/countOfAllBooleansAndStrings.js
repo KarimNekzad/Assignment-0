884f7fe846e0f2336df45af903bb3f3deb880a12
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 17:47:22 -0500

    Karim Nekzad 05-countOfAllBooleansAndStrings completed

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,8 @@
-function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
-}
+// Karim Nekzad
+let countOfAllBooleansAndStrings = arr => arr.filter(element => element === true
+                                                     || element === false
+                                                     || typeof element === 'string'
+                                                     || element instanceof String).length
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
