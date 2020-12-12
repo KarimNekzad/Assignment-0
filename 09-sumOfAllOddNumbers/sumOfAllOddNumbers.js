b204a4b59f6db74300528673e28607c0050b0217
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 18:59:20 -0500

    Karim Nekzad 09-sumOfAllOddNumbers completed

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,6 @@
-function sumOfAllOddNumbers(nums) {
-  // Insert code here;
-}
+// Karim Nekzad
+// again, not really a sum, just a count of the odd numbers
+let sumOfAllOddNumbers = nums => nums.filter(number => number % 2 !== 0).length
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
