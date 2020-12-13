ca59d3e7b08971069e948362bfe6d3b63e2dbe25
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 19:12:58 -0500

    Karim Nekzad 10-sumOfNumsWithinARange completed

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,7 @@
-function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
-}
+// Karim Nekzad
+// again! this is not a sum! it is a count!
+let sumOfNumsWithinARange = (nums, start, end) => nums.filter(number => number >= start
+                                                              && number <= end).length
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
