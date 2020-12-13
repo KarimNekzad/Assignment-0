6f9b56bb53bfdb857e450166d4a05c2f57d7b522
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 23:33:42 -0500

    Karim Nekzad 16-productOfAnyAmountOfNumbers completed

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,19 @@
-function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+// Karim Nekzad
+let productOfAnyAmountOfNumbers = (...args) => {
+  // spread our arguments in '...args' into an array
+  let nums = ([...args])
+
+  let result = product(nums, 0)
+  return result
+}
+
+let product = (arr, index) => {
+  // base case: final index of array reached
+  if (index === arr.length - 1) {
+    return arr[arr.length-1]
+  } else {
+    return arr[index] * product(arr, index+1)
+  }
 }
 
 // Do not edit this line;
