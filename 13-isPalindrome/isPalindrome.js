c2c3720bc15084cfcce621e99ec5f15636c89af7
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 20:28:28 -0500

    Karim Nekzad 13-isPalindrome completed

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,15 @@
-function isPalindrome(word) {
-  // Insert code here;
+// Karim Nekzad
+let isPalindrome = word => {
+  word = word.toLowerCase().split('') // input: Horse Output:  [ 'h', 'o', 'r', 's', 'e' ]
+
+  for (let i = 0; i < word.length/2; i++) {
+    let endIndex = word.length - 1
+    if (word[i] !== word[endIndex-i]) {
+      return false
+    }
+  }
+
+  return true
 }
 
 // Do not edit this line;
