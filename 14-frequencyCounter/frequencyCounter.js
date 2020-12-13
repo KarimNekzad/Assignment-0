106ff04451f576a8f88cca238cb653b2be5c542f
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 21:36:46 -0500

    Karim Nekzad 14-frequencyCountercompleted

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,16 @@
-function frequencyCounter(word) {
-  // Insert code here;
+// Karim Nekzad
+let frequencyCounter = word => {
+  let frequency = {}
+  for (let i=0; i<word.length; i++) {
+    let character = word[i]
+    if(frequency[character]) { // if undefined, returns false, if letter encounterd before, returns frequency, which === true
+      frequency[character]++
+    } else {
+      frequency[character] = 1 // starting value
+    }
+  }
+
+  return frequency
 }
 
 // Do not edit this line;
