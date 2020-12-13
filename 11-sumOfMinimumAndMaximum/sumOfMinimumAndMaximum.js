f43d8775a600a934831a52b043a1a74ac4c5fbf5
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 19:18:25 -0500

    Karim Nekzad 11-sumOfMinimumAndMaximum completed

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,22 @@
-function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+// Karim Nekzad
+let sumOfMinimumAndMaximum = nums => {
+  let min = 9999999
+
+  for (number of nums) {
+    if (number < min) {
+      min = number
+    }
+  }
+
+  let max = -9999999
+
+  for (number of nums) {
+    if (number > max) {
+      max = number
+    }
+  }
+
+  return min + max
 }
 
 // Do not edit this line;
