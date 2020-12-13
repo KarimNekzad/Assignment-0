875614c0d8c738ddb967bf82d6008ac6b41184ab
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-13 14:45:46 -0500

    Karim Nekzad 19-binarySearch completed

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,11 +1,23 @@
+// Karim Nekzad
 class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    if(nums.length === 1 && nums[0] != target) {
+      return false
+    }
+    let mid = Math.floor(nums.length/2) // If length = 11, return 5
+    if(nums[mid] === target ) {
+      return true
+    } else if (nums[mid] > target) {
+      return this.binarySearch(nums.slice(0, mid), target)
+    } else if (nums[mid] < target) {
+      return this.binarySearch(nums.slice(mid), target)
+    }
   }
+  
 }
 
 // Do not edit this line;
