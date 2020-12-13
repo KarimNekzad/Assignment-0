e1a1666cc87d41d9defadb9b972f155f665fd739
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 19:30:57 -0500

    Karim Nekzad 12-fizzBuzz completed

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,23 @@
-function fizzBuzz(start, end) {
-  // Insert code here;
+// Karim Nekzad
+let fizzBuzz = (start, end) => {
+  let nums = []
+
+  for (let i = start; i <= end; i++) {
+    nums.push(i)
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentNumber = nums[i]
+    if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+      nums[i] = 'FizzBuzz'
+    } else if (currentNumber % 3 === 0) {
+      nums[i] = 'Fizz'
+    } else if (currentNumber % 5 === 0) {
+      nums[i] = 'Buzz'
+    }
+  }
+
+  return nums
 }
 
 // Do not edit this line;
