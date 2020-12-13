9a54442c1ffc29e7d5fb3d91755a8a4631fc8363
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-12 23:22:55 -0500

    Karim Nekzad 15-countDownSum completed

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,11 @@
+// Karim Nekzad
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if (num == 1) {
+      return 1
+    } else {
+      return num + this.countDownSum(num-1)
+    }
   }
 }
 
