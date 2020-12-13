9d47cb28ebc526e365af1454fd6d07bf75076581
Karim Nekzad <karimnekzad00@gmail.com>
2020-12-13 11:22:35 -0500

    Karim Nekzad 17-setUnionOfAnyAmountOfSets completed

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,19 @@
+// Karim Nekzad
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let sets = [...args]
+  let masterSet = new Set()
+  
+  for (set of sets) {
+    for (number of set) {
+      masterSet.add(number)
+    } 
+  }
+  
+  return masterSet
 }
+
+// setUnionOfAnyAmountOfSets({ 1, 2, 4 }, { 4, 5, 6 }, { 6, 7, 8 }) //given e.g. in .md file
+setUnionOfAnyAmountOfSets([1, 2, 4], [4, 5, 6], [6, 7, 8])
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
